8e06a50a9ffa41ee7b5c58d2be2ea316266cbaab
Marcos B <marco.beatrice8@gmail.com>
2023-11-10 15:16:29 +0000

    Adding Terraform Clpud organization and workspace

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "Beamar_org"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
